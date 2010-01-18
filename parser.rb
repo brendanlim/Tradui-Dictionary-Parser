3fdebc0f37ad7ddee305fce6e7d261cd4a61d551
Brendan G. Lim <brendangl@gmail.com>
2010-01-18 15:27:32 -0500

    Fixing odd reversal issue

--- parser.rb
+++ parser.rb
@@ -8,12 +8,17 @@ translations = [];
 (doc/:term).each do |xml_term|
   creole = xml_term.attributes["word"]
   translations.push({"#{creole}" => xml_term.search("/value").collect{|x| x.innerHTML}})
+  break if creole == 'zwazo'
 end
 
+count = 0
 translations.each do |hash_values|
   hash_values.each_pair do |key,values|
-    values.each do |value|    
+    values.each do |value|
       p "INSERT INTO DICTIONARY (CREOLE,ENGLISH) VALUES ('#{key}','#{value}');"
     end
   end
 end
+
+p translations.size
+p count
