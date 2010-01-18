ee23fe89da7244204505bb4c8aff86c96f52ed9d
Brendan G. Lim <brendangl@gmail.com>
2010-01-18 12:17:46 -0500

    Adding in the parser

---
+++ parser.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'open-uri'
+require 'hpricot'
+
+doc = Hpricot(File.read("dictionary.xml"))
+
+translations = [];
+(doc/:term).each do |xml_term|
+  creole = xml_term.attributes["word"]
+  translations.push({"#{creole}" => xml_term.search("/value").collect{|x| x.innerHTML}})
+end
+
+translations.each do |hash|
+  hash.each_pair do |key,values|
+    values.each do |value|    
+      p "INSERT INTO DICTIONARY (CREOLE,ENGLISH) VALUES ('#{key}','#{value}');"
+    end
+  end
+end
